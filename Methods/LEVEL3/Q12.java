3cd815d6da7a37b4c4335c556737ed1f51379f52
akap29 <purwar.akarsha29@gmail.com>
2025-08-31 23:25:30 +0530

    Update Q12.java

---
+++ Methods/LEVEL3/Q12.java
@@ -0,0 +1,32 @@
+package Methods.LEVEL3;
+import java.util.Scanner;
+
+public class Q12{
+
+    public static double euclideanDistance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+    public static double[] lineEquation(int x1, int y1, int x2, int y2) {
+        double m = (double)(y2 - y1) / (x2 - x1);  
+        double b = y1 - (m * x1);                 
+        return new double[]{m, b};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter x1 y1: ");
+        int x1 = sc.nextInt(), y1 = sc.nextInt();
+
+        System.out.print("Enter x2 y2: ");
+        int x2 = sc.nextInt(), y2 = sc.nextInt();
+
+        double distance = euclideanDistance(x1, y1, x2, y2);
+
+        double[] line = lineEquation(x1, y1, x2, y2);
+
+        System.out.println("\nEuclidean Distance: " + distance);
+        System.out.printf("Equation of line: y = %.2fx + %.2f\n", line[0], line[1]);
+    }
+}
